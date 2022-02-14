e3a699ef281d989c5d6b2f5e53fa1b28b7c1669e
Jeong-DongHee <weg9511@gmail.com>
2022-02-14 16:05:39 +0900

    스프링 부트, 웹 MVC, DB 접근 기술, AOP 실습

---
+++ src/main/java/hello/hellospring/donmain/Member.java
@@ -0,0 +1,29 @@
+package hello.hellospring.donmain;
+
+
+import javax.persistence.*;
+import javax.swing.plaf.basic.BasicDesktopIconUI;
+
+@Entity
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
